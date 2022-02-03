50fe1b03cf63ae1c6219e891a5da50b113042cc1
StraysDefi <strays33defi@gmail.com>
2022-02-03 10:28:14 +0100

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,26 @@
+from brownie import FundMe
+from scripts.helpers import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+    print(f"Entry fee: {entrance_fee}")
+    print("Funding...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+    print("Funding successful.")
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    print("Withdrawing...")
+    fund_me.withdraw({"from": account})
+    print("Withdrawal successful.")
+
+
+def main():
+    fund()
+    withdraw()
